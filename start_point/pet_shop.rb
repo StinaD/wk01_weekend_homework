eb9077094e4df00c674b18daf5c7d0686b687c9d
Christina Davies <christinahdavies@gmail.com>
2018-10-22 09:07:01 +0100

    Additional refactoring

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -1,31 +1,31 @@
 #1
 def pet_shop_name(shop)
-  return shop[:name]
+  shop[:name]
 end
 
 #2
 def total_cash(shop)
- return shop[:admin][:total_cash]
+  shop[:admin][:total_cash]
 end
 
 #3 and 4
 def add_or_remove_cash(shop, amount)
-  return new_balance = shop[:admin][:total_cash] += amount
+  new_balance = shop[:admin][:total_cash] += amount
 end
 
 # 5
 def pets_sold(shop)
- return shop[:admin][:pets_sold]
+ shop[:admin][:pets_sold]
 end
 
 # 6
 def increase_pets_sold(shop, number_of_pets_sold)
-  return shop[:admin][:pets_sold] += number_of_pets_sold
+  shop[:admin][:pets_sold] += number_of_pets_sold
 end
 
 # 7
 def stock_count(shop)
-  return shop[:pets].length
+  shop[:pets].length
 end
 
 #8
@@ -62,30 +62,30 @@ end
 
 # 13
 def customer_cash(customer)
-  return customer[:cash]
+  customer[:cash]
 end
 
 
 # 14
 def remove_customer_cash(customer, amount)
-  return customer[:cash] -= amount
+  customer[:cash] -= amount
 end
 
 
 # 15
 def customer_pet_count(customer)
-  return customer[:pets].length
+  customer[:pets].length
 end
 
 
 # 16a
 def add_pet_to_customer(customer, new_pet)
-  return customer[:pets].push(new_pet)
+  customer[:pets].push(new_pet)
 end
 
 # 16b
 def test_customer_pet_count(customer)
-  return customer[:pets].length
+  customer[:pets].length
 end
 
 
@@ -93,7 +93,7 @@ end
 
 # 17 and 18
 def customer_can_afford_pet(customer, pet)
-  return customer[:cash] >= pet[:price]
+  customer[:cash] >= pet[:price]
 end
 
 
